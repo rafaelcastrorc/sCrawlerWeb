e324afbfe546eb873599eb753c21a25dd56c4a3b
rafaelcastro <rafc@seas.upenn.edu>
2018-03-05 10:49:27 -0500

    Basic app working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { ScrawlersComponent } from './scrawlers/scrawlers.component';
+import {FormsModule} from "@angular/forms";
+import { DropdownModule } from 'ngx-dropdown';
+import { HttpClientModule } from '@angular/common/http';
+import { NgHttpLoaderModule } from "ng-http-loader/ng-http-loader.module";
+import { ChartsModule } from 'ng2-charts';
+import { ScrawlerListComponent } from './scrawlers/scrawler-list/scrawler-list.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScrawlersComponent,
+    ScrawlerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    DropdownModule,
+    HttpClientModule,
+    NgHttpLoaderModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
